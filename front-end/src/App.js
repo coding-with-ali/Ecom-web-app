3dc4ea90285ff0b99e1f0aa32ebca563471b2f88
coding-with-ali <haxratali0@gmail.com>
2022-07-21 18:49:21 +0500

    adding initial files

---
+++ front-end/src/App.js
@@ -0,0 +1,39 @@
+import Container from 'react-bootstrap/Container'
+import Footer from './components/Footer';
+import Header from './components/Header';
+import './index.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+import CartScreen from './screens/CartScreen';
+import LoginScreen  from './screens/LoginScreen';
+import RegisterScreen  from './screens/RegisterScreen';
+import ProfileScreen  from './screens/ProfileScreen';
+import ShippingScreen  from './screens/ShippingScreen';
+
+function App() {
+  return (
+    <Router>
+      <Header/>
+      <main className='py-3'>
+        <Container >
+         <Routes>
+           <Route path="/" exact element={<HomeScreen/>} />
+           <Route path="/login"  element={<LoginScreen/>} />
+           <Route path="/register"  element={<RegisterScreen/>} />
+           <Route path="/profile"  element={<ProfileScreen/>} />
+           <Route path="/shipping"  element={<ShippingScreen/>} />
+            {/*  I am passing id as a param */}
+           <Route path="/product/:id"  element={<ProductScreen/>} />
+           <Route path="/cart/"  element={<CartScreen/>} >
+              <Route path=":params" element={<CartScreen />} />
+            </Route>
+         </Routes>
+        </Container>
+      </main>
+      <Footer/>
+    </Router>
+  );
+}
+
+export default App;
