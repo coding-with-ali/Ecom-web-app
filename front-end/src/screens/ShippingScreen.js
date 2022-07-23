2e43d337a3e20ed878f3b4020da03b638a6358d7
coding-with-ali <haxratali0@gmail.com>
2022-07-23 17:44:13 +0500

    created the Navbar
    so it is just to track the steps of the ordr

--- front-end/src/screens/ShippingScreen.js
+++ front-end/src/screens/ShippingScreen.js
@@ -5,6 +5,7 @@ import FormCotainer from '../components/FormContainer';
 import {useDispatch, useSelector} from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import {saveShippingAddress} from '../actions/cartActions'
+import CheckOutSteps from '../components/CheckOutSteps';
 
 
 function ShippingScreen() {
@@ -26,6 +27,7 @@ function ShippingScreen() {
   }
   return (
     <FormCotainer> 
+      <CheckOutSteps step1 step2/>
       <h1>Shipping</h1>
       <Form onSubmit={submitHandler}>
       <Form.Group controlId = 'address'>
