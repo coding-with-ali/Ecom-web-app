b6b52e941a815b9d5b22139c698aa4b39b294686
coding-with-ali <haxratali0@gmail.com>
2022-07-31 19:50:30 +0500

    changing the config

--- backend/base/urls/order_urls.py
+++ backend/base/urls/order_urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-from backend.base.views.order_views import getOrderById
 from base.views import order_views as  views
 
 
 
 urlpatterns=[
-    path('add/',views.addOrderItems, name='orders-add')
+    path('add/',views.addOrderItems, name='orders-add'),
     path('<str:pk>/',views.getOrderById, name='user-order')
 
 ]
--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,3 +1,5 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
 import Container from 'react-bootstrap/Container'
 import Footer from './components/Footer';
 import Header from './components/Header';
